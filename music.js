746dcce9823411ecc253ef511ef3f3e0c8a2feab
rachit <curiousrachit.26@gmail.com>
2023-06-17 21:08:30 +0530

    add more data

--- music.js
+++ music.js
@@ -1,11 +1,79 @@
-const Music=document.querySelector('.audio');
-const Img=document.querySelector('.rotate-image');
 
-const Play=document.querySelector('#play-button');
-Play.addEventListener("click", ()=>{
-    Music.play();
-Play.classList.remove("#play-button");
-    Img.classList.add('anime');
+// /.........for playing................
+const PlayButton = document.querySelector("#play-button");
+const AudioPlay = document.querySelector(".audio");
+
+// /.........for pausing................
+const PauseButton = document.querySelector("#pause-button");
 
 
-  })
+
+
+// ..............change song detail.........
+const SongName = document.querySelector(".song-name");
+const SingerName = document.querySelector(".singer-name");
+const RotateImage=document.querySelector(".rotate-image");
+// ...................left button.......
+
+const LeftButton = document.querySelector("#left-button")
+
+
+
+// ...................right button.......
+
+const RightButton = document.querySelector("#right-button")
+
+
+
+// ...........data of song
+const songs = [
+    {
+        SongName: "never let you go",
+        SingerName: "Zyan Malik",
+        RotateImage:""
+    },
+    {
+        SongName: "let me love you",
+        SingerName: "Justine ",
+
+    },
+    {
+        SongName: "mistileo ",
+        SingerName: "Justine bieber",
+
+    },
+
+];
+
+
+const LoadSong=(songs)=>{
+    SongName.textContent=songs.SongName;
+    SingerName.textContent=songs.SingerName;
+
+}
+
+
+SongIndex=0;
+const NextSong=()=>{
+    SongIndex=(SongIndex+1)% songs.length;
+    LoadSong(songs[SongIndex]);
+}
+
+const PreveSong=()=>{
+    SongIndex=(SongIndex-1);
+
+    LoadSong(songs[SongIndex]);
+
+}
+RightButton.addEventListener("click",NextSong);
+LeftButton.addEventListener("click",PreveSong);
+
+
+PauseButton.addEventListener("click", () => {
+    AudioPlay.pause();
+})
+
+
+PlayButton.addEventListener('click', () => {
+    AudioPlay.play();
+})
