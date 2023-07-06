d2cff03490855e45ee392b5ffb61eb759a6cb2f2
rachit <curiousrachit.26@gmail.com>
2023-07-06 23:11:12 +0530

    music app

--- music.js
+++ music.js
@@ -1,79 +1,28 @@
+const pauseButton=document.getElementById("pause-button");
+const playButton=document.getElementById("play-button");
 
-// /.........for playing................
-const PlayButton = document.querySelector("#play-button");
-const AudioPlay = document.querySelector(".audio");
+const control=document.getElementById("control");
 
-// /.........for pausing................
-const PauseButton = document.querySelector("#pause-button");
-
-
-
-
-// ..............change song detail.........
-const SongName = document.querySelector(".song-name");
-const SingerName = document.querySelector(".singer-name");
-const RotateImage=document.querySelector(".rotate-image");
-// ...................left button.......
-
-const LeftButton = document.querySelector("#left-button")
+const rightButton=document.getElementById("right-button");
+const leftButton=document.getElementById("left-button");
 
 
 
-// ...................right button.......
-
-const RightButton = document.querySelector("#right-button")
+const audio=document.getElementById("audio");
 
 
-
-// ...........data of song
-const songs = [
-    {
-        SongName: "never let you go",
-        SingerName: "Zyan Malik",
-        RotateImage:""
-    },
-    {
-        SongName: "let me love you",
-        SingerName: "Justine ",
-
-    },
-    {
-        SongName: "mistileo ",
-        SingerName: "Justine bieber",
-
-    },
-
-];
-
-
-const LoadSong=(songs)=>{
-    SongName.textContent=songs.SongName;
-    SingerName.textContent=songs.SingerName;
-
-}
-
-
-SongIndex=0;
-const NextSong=()=>{
-    SongIndex=(SongIndex+1)% songs.length;
-    LoadSong(songs[SongIndex]);
-}
-
-const PreveSong=()=>{
-    SongIndex=(SongIndex-1);
-
-    LoadSong(songs[SongIndex]);
-
-}
-RightButton.addEventListener("click",NextSong);
-LeftButton.addEventListener("click",PreveSong);
-
-
-PauseButton.addEventListener("click", () => {
-    AudioPlay.pause();
+playButton.addEventListener("click",function(){
+audio.play()
 })
 
 
-PlayButton.addEventListener('click', () => {
-    AudioPlay.play();
-})
+pauseButton.addEventListener("click",function(){
+    audio.pause()
+    })
+
+
+    leftButton.addEventListener("click",function(){
+        
+        })
+    
+    
