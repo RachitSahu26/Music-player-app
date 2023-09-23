6f2a7eeec541680e0feb2a940d348c4cd7920c9a
rachit <curiousrachit.26@gmail.com>
2023-09-23 12:57:30 +0530

    Playing ,Pause, Left , Right function completed

--- music.js
+++ music.js
@@ -3,18 +3,117 @@
 const playButton = document.getElementById("play-button");
 
 const control = document.getElementById("control");
+
+
 // ......right btn.....
-
 const rightButton = document.getElementById("right-button");
-// ......left btn.....
 
+
+
+// ......left btn.....
 const leftButton = document.getElementById("left-button");
 
 
 
-const audio = document.getElementById("audio");
+// ......song name .....
+let songN = document.getElementById("song-name");
 
-const listOfSong=["./Let Me .mp3","./main tera hero.mp3","./Salamat.mp3","./Stranger.mp3"];
+
+
+
+// ......singer name .....
+let singerN = document.getElementById("singer-name");
+
+// ....changing image ....
+let ChangeImg = document.getElementsByClassName("img-area");
+
+
+const music = document.querySelector("audio");
+
+const listOfSong = [
+ 
+//  ...............1 song...........
+    {
+        songUrl: "Let Me ",
+        singerName: "Zyan Malik",
+        songName: "Let Me",
+
+    },
+//  ...............2 song...........
+
+    {
+        songUrl: "main tera hero",
+        singerName: "Arman Malik",
+        songName: "Mai Tera Hero",
+
+    },
+
+
+//  ...............3 song...........
+
+   
+
+
+    {
+        songUrl: "Salamat",
+        singerName: "Rachit Sahu",
+        songName: "Salamat Rahe",
+
+    },
+//  ...............4 song...........
+
+
+    {
+        songUrl: "Stranger",
+        singerName: "Sadie Sink",
+        songName: "Stranger Thing",
+
+    },
+
+//  ...............5 song...........
+
+
+{
+    songUrl: "Doctor_Strange___Believer(256k)",
+    singerName: "Rachit",
+    songName: "Believer",
+
+},
+//  ...............6 song...........
+
+
+{
+    songUrl: "NEFFEX_-_Cold_❄️[Copyright_Free](256k)",
+    singerName: "Rachit Favourite",
+    songName: "Cold",
+
+},
+
+//  ...............7 song...........
+
+
+{
+    songUrl: "Kidi_Touch_it_Ringtone____Touch_It%21_Shut_Up%21_ringtone____Kidi_-_Touch_It_New_Remix_Ringtone____reels(256k)",
+    singerName: "No Singer",
+    songName: "Kidi_Touch",
+
+},
+
+// /  ...............7 song...........
+
+
+{
+    songUrl: "Baby",
+    singerName: "Justine_Bieber",
+    songName: "Baby",
+
+},
+
+
+
+
+
+];
 
 
 
@@ -28,37 +127,64 @@ const listOfSong=["./Let Me .mp3","./main tera hero.mp3","./Salamat.mp3","./Stra
 
 // ............play btn functionality........
 
-let msuicImg=true;
+let isPlaying = true;
+const playMus = () => {
 
-    playButton.addEventListener("click", function () {
-if(msuicImg){
-
-    audio.src=listOfSong[0];
+     isPlaying = false;
+    music.play();
     playButton.src = "./pause.png";
-    msuicImg=false;
-}else{
-   
-    audio.pause();
-    playButton.src = "./play-button.png"; 
-    msuicImg=true;
 
 }
-    })
-    musicSong = 0;
+const pauseMus = () => {
+   isPlaying = true;
 
-  
-
-
-
+    music.pause();
+    playButton.src = "./play-button.png";
 
 
 
-// ............left btn functionality........
+}
+
+playButton.addEventListener("click", () => {
+    if (isPlaying) {
+       playMus();
+    } else {
+        pauseMus();
+    }
 
 
-    leftButton.addEventListener("click",function(){
-
-    } )
+})
 
 
+// ............right btn functionality........
 
+
+
+const musicLoad=(listOfSong)=>{
+    music.src="./"+listOfSong.songUrl +".mp3";
+
+    singerN.textContent=listOfSong.singerName;
+    songN.textContent=listOfSong.songName;
+    
+
+}
+
+
+let musicIndex=0;
+rightButton.addEventListener("click", () => {
+    
+    musicIndex=(musicIndex+1)%listOfSong.length;
+    musicLoad(listOfSong[musicIndex]); // Example: Load the third song from the list
+    playButton.src = "./pause.png";
+    music.play();
+
+});
+
+// ............right btn functionality........
+leftButton.addEventListener("click", () => {
+    musicIndex=(musicIndex-1+listOfSong.length)%listOfSong.length;
+    musicLoad(listOfSong[musicIndex]); // Example: Load the third song from the list
+    playButton.src = "./pause.png";
+   
+    music.play();
+});
